098bf7377ff9fd85fdf5b50556cbce3e6e1c6330
Sane Generous <karim2k11@gmail.com>
2024-06-11 16:05:15 +0100

    Initial project add.

---
+++ src/main/java/tp/msk/msscbreweyclient/MsscBreweyClientApplication.java
@@ -0,0 +1,13 @@
+package tp.msk.msscbreweyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweyClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBreweyClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tp/msk/msscbreweyclient/MsscBreweyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.msk.msscbreweyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweyClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
